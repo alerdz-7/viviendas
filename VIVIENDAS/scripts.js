6dac37a70cd6896851c190a3152c1b01beabab3f
alerdz-7 <alejandroloera195@gmail.com>
2025-02-16 14:27:21 -0600

    Delete VIVIENDAS directory

--- VIVIENDAS/scripts.js
+++
@@ -1,8 +0,0 @@
-// Seleccionar todos los botones con la clase "cta-button"
-document.querySelectorAll('.cta-button').forEach(function(button) {
-    button.addEventListener('click', function(event) {
-        
-        const href = button.getAttribute('href');
-        window.location.href = href;
-    });
-});
